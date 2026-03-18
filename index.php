251a758c48fdc90f9cb459fdfdc41a1912c5d521
MikiSanya <115850013+MikiSanyaX@users.noreply.github.com>
2026-03-18 14:05:28 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Главная страница</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>Список страниц</h1>
+    <ul>
+        <li><a href="page1.php">Страница 1 (404 Not Found)</a></li>
+        <li><a href="page2.php?text=Hello%20World">Страница 2 (скачать файл с текстом)</a></li>
+        <li><a href="page3.php">Страница 3 (счётчик и редирект)</a></li>
+        <li><a href="page4.php">Страница 4 (показать счётчик)</a></li>
+    </ul>
+</body>
+</html>
